e980488cc2762377e4a4c2115bdb1ddb26fad86f
wuqi <1119171575@qq.com>
2018-10-12 16:34:10 +0800

    新增倒金字塔输出功能

--- PyramidDemo.java
+++ PyramidDemo.java
@@ -1,4 +1,4 @@
-package wq.demo;
+package feihong.demo;
 
 import java.util.Scanner;
 
@@ -6,11 +6,9 @@ public class PyramidDemo {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("请输入金字塔层数：");
-
 		int n = scanner.nextInt();
 		System.out.print("正金字塔输入1，倒金字塔输入2：");	
 		int m = scanner.nextInt();
-
 		switch (m) {
 			case 1:
 				straight(n);
@@ -23,7 +21,10 @@ public class PyramidDemo {
 				break;
 		}
 	}
-
+	
+	/**
+	 * 正序输出
+	 */
 	public static void straight(int n) {
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= 2*n-1; j++) {
@@ -42,4 +43,23 @@ public class PyramidDemo {
 		
 	}
 	
+	/**
+	 * 倒序输出
+	 */
+	public static void fall(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= 2*n-1; j++) {
+				if (j>2*n-i) {
+					break;
+				}
+				if (j<=i-1) {
+					System.out.print(' ');
+				}
+				if (j>i-1) {
+					System.out.print('*');	
+				}		
+			}
+			System.out.println();
+		}
+	}
 }
